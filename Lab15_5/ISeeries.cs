dca420064006b35565375fe7c561bb758defb69f
AntoninaVish <vishivtonya@gmail.com>
2022-01-09 00:11:59 +0300

    Добавьте файлы проекта.

---
+++ Lab15_5/ISeeries.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15_5
+{
+    /*
+       метод void setStart(int x) - устанавливает начальное значение
+       метод int getNext() - возвращает следующее число ряда
+       метод void reset() - выполняет сброс к начальному значению
+       
+    */
+
+    interface ISeries
+    {
+        void setStart(int x); // принимает аргумент Х и ничего не возвращает
+        int getNext(); //должен возвращать следующее число
+        void reset(); //делает сброс с начальным значением
+    }
+
+    class ArithProgression : ISeries 
+    {
+        int step;// это шаг
+        int startValue;
+        int currentValue;
+        public int getNext() //метод
+        {
+            currentValue+=step; // текущее значение (числу) увеличиваем на шаг
+            return currentValue; //возвращаем текущее значение (число)
+        }
+        public void reset()
+        {
+            currentValue=startValue;// устанавливаем currentValue в startValue
+        }
+
+        public void setStart(int x)
+        {
+            startValue=x;
+            currentValue = startValue; //текущее значение равно стартовому значению
+        }
+        public void setStep(int s) // метод
+        {
+            step = s; // заносим S
+        }
+        
+    }
+    class GeomProgression: ISeries
+    {
+        int step;// это шаг
+        int startValue;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue *= step; // текущее значение (число) умножаем на шаг
+            return currentValue;  //возвращаем текущее значение (число)
+        }
+        public void reset()
+        {
+            currentValue = startValue;// устанавливаем currentValue в startValue
+        }
+
+        public void setStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;// текущее значение равно стартовому значению
+        }
+        public void setStep(int s) // метод
+        {
+            step = s; // заносим S
+        }
+
+    }
+}
---
+++ Lab15_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();//создаем арифметическую прогрессию, экземпляр класса
+            arith.setStart(10);//устанавливаем стартовое значение 10
+            arith.setStep(2);  // устанавливаем значения шага 2
+            Console.WriteLine(arith.getNext());// вывод первого члена арифметической прогрессии
+            Console.WriteLine(arith.getNext());// вывод второго члена арифметической прогрессии
+            Console.WriteLine(arith.getNext());// вывод третьего члена арифметической прогрессии
+            arith.reset();// сброс до 10
+            Console.WriteLine(arith.getNext());// выводим на экран арифметическое значение метода
+            Console.WriteLine(arith.getNext());// выводим на экран арифметическое значение метода
+                       
+            GeomProgression arith1 = new GeomProgression();//создаем геометрическую прогрессию, экземпляр класса
+            arith1.setStart(8);//устанавливаем стартовое значение 8
+            arith1.setStep(2); // устанавливаем значения шага 2
+            Console.WriteLine(arith1.getNext());// вывод первого члена геометрической прогрессии
+            Console.WriteLine(arith1.getNext());// вывод второго члена геометрической прогрессии
+            Console.WriteLine(arith1.getNext());// вывод третьего члена геометрической прогрессии
+            arith1.reset();// сброс до 8
+            Console.WriteLine(arith1.getNext());// выводим на экран геометрическое значение метода
+            Console.WriteLine(arith1.getNext());// выводим на экран геометрическое значение метода
+            Console.ReadKey();
+        }
+    }
+}
